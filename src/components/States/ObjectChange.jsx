a25d8abdf98cf31b12c381585cac1fd06eacd375
bndnt <67003594+bndnt@users.noreply.github.com>
2025-08-12 20:33:39 -0400

    Хук useEffect

--- src/components/States/ObjectChange.jsx
+++ src/components/States/ObjectChange.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 export default function ObjectChange() {
   const [values, setValues] = useState({
     x: 0,
@@ -16,6 +16,18 @@ export default function ObjectChange() {
       y: values.y + 1,
     });
   };
+
+  useEffect(() => {
+    console.log("First updated: ", values.x);
+  }, [values.x]);
+
+  useEffect(() => {
+    console.log("Y updated:", values.y);
+  }, [values.y]);
+
+  useEffect(() => {
+    console.log("X and Y updated:", values.x, values.y);
+  }, [values.x, values.y]);
   return (
     <div>
       <p>
